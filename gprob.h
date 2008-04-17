6782eace2eaeca76ac3d0aa575d6a7c44da6dc8f
Josh Triplett <josh@freedesktop.org>
2008-04-17 10:17:25 -0700

    Remove trailing whitespace from gprob.h

--- gprob.h
+++ gprob.h
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2007 Bart Massey
  * ALL RIGHTS RESERVED
- * 
+ *
  * This program is licensed under the GPL version 2 or GPL
  * version 3 or later.  Please see the file COPYING in the
  * source distribution of this software for license terms.
