c72129fb50aa34c6dd5f97a500af57e8f4672ad4
Julian Blake Kongslie <jblake@omgwallhack.org>
2008-05-29 16:50:19 -0700

    Control decisions.

---
+++ control.c
@@ -0,0 +1,19 @@
+// Copyright 2008 Julian Blake Kongslie
+// Licensed under the GNU GPL version 2.
+
+#include "rocket.h"
+
+bool detect_apogee_in_coast( struct rocket *state ) {
+  return  (   state->state == STATE_COAST
+          &&  state->position.z > 500
+          &&  state->velocity.z <= 5
+          );
+};
+
+bool detect_500m_in_fall( struct rocket *state ) {
+  return  (   (   state->state == STATE_COAST
+              ||  state->state == STATE_DROGUECHUTE
+              )
+          &&  state->position.z <= 500
+          );
+};
---
+++ control.h
@@ -0,0 +1,9 @@
+// Copyright 2008 Julian Blake Kongslie
+// Licensed under the GNU GPL version 2.
+
+#pragma once
+
+#include "rocket.h"
+
+bool detect_apogee_in_coast( struct rocket *state );
+bool detect_500m_in_fall( struct rocket *state );
