200f415a0bc4915efe43008107f7622e0d92fc67
Julian Blake Kongslie <jblake@omgwallhack.org>
2008-05-29 16:21:17 -0700

    Copied Holly Grimes' pressure sensor code from the event-driven-fc code.

---
+++ sensor/pressure_sensor.h
@@ -0,0 +1,21 @@
+/*
+ * Header file for use with Pressure Sensor Model, version 1.1
+ *
+ * Written by Holly Grimes
+ */
+
+#ifndef PRESSURE_SENSOR_H
+#define PRESSURE_SENSOR_H
+
+#define GRAVITATIONAL_ACCELERATION -9.80665
+#define AIR_GAS_CONSTANT 287.053
+#define NUMBER_OF_LAYERS 7
+#define MAXIMUM_ALTITUDE 84852
+#define MINIMUM_PRESSURE 0.3734
+#define LAYER0_BASE_TEMPERATURE 288.15
+#define LAYER0_BASE_PRESSURE 101325
+
+double altitude_to_pressure(double);
+double pressure_to_altitude(double);
+
+#endif
