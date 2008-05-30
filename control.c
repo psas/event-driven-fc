e9477931adaaf31010494596b522b4442c8a5be1
Julian Blake Kongslie <jblake@omgwallhack.org>
2008-05-29 17:57:01 -0700

    Flight controller.

---
+++ control.c
@@ -0,0 +1,95 @@
+// Copyright 2008 Julian Blake Kongslie
+// Licensed under the GNU GPL version 2.
+
+#include "control.h"
+#include "filter.h"
+#include "flightsim.h"
+#include "query.h"
+#include "rocket.h"
+#include "sensor.h"
+#include "sensor/accelerometer.h"
+#include "sensor/pressure_sensor.h"
+
+void run_flight_control ( void ) {
+
+  struct particle filter[2][PARTICLE_COUNT];
+  unsigned int    which_filter = 0;
+
+  struct flightsim_state  flightsim = initial_sim;
+
+  double  time_until_accelerometer    = ACCELEROMETER_FREQ;
+  double  time_until_pressume_sensor  = PRESSURE_SENSOR_FREQ;
+
+  // The test functions.
+  double test_accelerometer( struct rocket *state ) {
+    return prob_given( state->accel.z, flightsim->rocket.accel.z, ACCELEROMETER_NOISE_SIGMA );
+  };
+
+  double test_pressure_sensor( struct rocket *state ) {
+    return prob_given( altitude_to_pressure( state->position.z ), altitude_to_pressure( flightsim->rocket.position.z ), PRESSURE_SENSOR_NOISE_SIGMA );
+  };
+
+  // Initialize all the particles to the launch state.
+  // If we want to be able to get a lock from scratch during mid-flight, we need a better distribution here.
+  for ( int i = 0; i < 2; ++i ) {
+    for ( int j = 0; j < PARTICLE_COUNT; ++j ) {
+      filter[i][j].weight = 1;
+      filter[i][j].state  =
+        { .state    = STATE_WAITING
+        , .position =
+          { .z      = 0
+          }
+        , .velocity =
+          { .z      = 0
+          }
+        , .accel    =
+          { .z      = 0
+          }
+        , .fuel     = FUEL_MASS
+        };
+    };
+  };
+
+  // This is the main control loop.
+  do {
+
+    // Update by the smaller of the two sensor times.
+    double delta_t = time_until_accelerometer < time_until_pressume_sensor ? time_until_accelerometer : time_until_pressume_sensor;
+
+    // Advance the flight simulator. Break out if it says the simulation has concluded.
+    if ( ! flightsim_tick( delta_t, &flightsim ) )
+      break;
+
+    // Summarize the current flight simulator state.
+    print_rocket( &(flightsim.rocket) );
+
+    // Advance the particle filter.
+    update_particles( delta_t, PARTICLE_COUNT, filter[which_filter] );
+
+    // Apply the relevant sensor updates.
+    double total_weight;
+
+    if ( delta_t >= time_until_accelerometer )
+      total_weight = test_particles( test_accelerometer, PARTICLE_COUNT, filter[which_filter] );
+    if ( delta_t >= time_until_pressure_sensor )
+      total_weight = test_particles( test_pressure_sensor, PARTICLE_COUNT, filter[which_filter] );
+
+    // Make control decisions.
+    if ( query_particles( detect_apogee_in_coast, PARTICLE_COUNT, filter[which_filter] ) ) {
+      printf( "release drogue chute\n" );
+      release_drogue_chute( &flightsim );
+    } else if ( query_particles( detect_500m_in_fall, PARTICLE_COUNT, filter[which_filter] ) ) {
+      printf( "release main chute\n" );
+      release_main_chute( &flightsim );
+    };
+
+    // Resample if we drop below threshold.
+    if ( total_weight < RESAMPLE_THRESHOLD ) {
+      printf( "resample\n" );
+      resample_particles( total_weight, PARTICLE_COUNT, filter[which_filter], PARTICLE_COUNT, filter[! which_filter] );
+      which_filter = ! which_filter;
+    };
+
+  };
+
+};
---
+++ control.h
@@ -0,0 +1,13 @@
+// Copyright 2008 Julian Blake Kongslie
+// Licensed under the GNU GPL version 2.
+
+#pragma once
+
+#define PARTICLE_COUNT  1000
+
+#define RESAMPLE_THRESHOLD  250
+
+#define ACCELEROMETER_FREQ    0.001
+#define PRESSURE_SENSOR_FREQ  0.1
+
+void run_flight_control ( void );
