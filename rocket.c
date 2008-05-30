96fb9153217cef8da26ee0b997c2aef8b75a59a8
Julian Blake Kongslie <jblake@omgwallhack.org>
2008-05-29 23:21:11 -0700

    Correct the physics model.

--- rocket.c
+++ rocket.c
@@ -17,6 +17,7 @@ static inline void incorporate_drag( struct rocket *rocket, double drag_coeff, d
         * rocket->velocity.z * rocket->velocity.z
         * drag_coeff
         * cross_section
+        / ( rocket->fuel + DRY_MASS )
     };
 
   vec_add( &(rocket->accel), &drag );
@@ -25,7 +26,7 @@ static inline void incorporate_drag( struct rocket *rocket, double drag_coeff, d
 
 static inline void incorporate_thrust( struct rocket *rocket, double thrust ) {
 
-  struct vec thrust_v = { .z = thrust };
+  struct vec thrust_v = { .z = thrust / ( rocket->fuel + DRY_MASS ) };
 
   vec_add( &(rocket->accel), &thrust_v );
 
@@ -39,25 +40,28 @@ void update_rocket( double delta_t, struct rocket *rocket ) {
   // Gravity applies to all states.
   rocket->accel.z = -EARTH_GRAVITY;
 
-  // Drag from the rocket applies to all states.
+  // Drag from the rocket body applies to all states.
   incorporate_drag( rocket, ROCKET_DRAG, ROCKET_CROSS_SECTION );
 
+  // Incorporate drag from chute or rocket.
+  if (rocket->state == STATE_DROGUECHUTE || rocket->state == STATE_MAINCHUTE)
+    incorporate_drag( rocket, DROGUE_CHUTE_DRAG, DROGUE_CHUTE_CROSS_SECTION );
+  if (rocket->state == STATE_MAINCHUTE)
+    incorporate_drag( rocket, MAIN_CHUTE_DRAG, MAIN_CHUTE_CROSS_SECTION );
+
   // In the burn state, we thrust and consume fuel. If all the fuel is consumed, we force ourselves into coast state.
+  // Note that because we do this after calculating drag, we underestimate the drag deacceleration during a burn.
+  // And the engine thrust is calculated after subtracting spent fuel, so it is overestimated.
   if (rocket->state == STATE_BURN) {
     rocket->fuel -= BURN_RATE * delta_t;
     if (rocket->fuel > 0)
       incorporate_thrust( rocket, ENGINE_THRUST );
     else {
+      rocket->fuel = 0;
       incorporate_thrust( rocket, ENGINE_THRUST * (BURN_RATE * delta_t + rocket->fuel) );
       rocket->state = STATE_COAST;
     };
   };
-
-  // Incorporate drag from chutes.
-  if (rocket->state == STATE_DROGUECHUTE || rocket->state == STATE_MAINCHUTE)
-    incorporate_drag( rocket, DROGUE_CHUTE_DRAG, DROGUE_CHUTE_CROSS_SECTION );
-  if (rocket->state == STATE_MAINCHUTE)
-    incorporate_drag( rocket, MAIN_CHUTE_DRAG, MAIN_CHUTE_CROSS_SECTION );
 
   // Integrate terms. I assume a linear transition from beginning of timestep to end of timestep.
   vec_add( &accel, &(rocket->accel) );
