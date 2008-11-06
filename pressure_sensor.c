ae228031dbe02cf7339cf71632ae7c2b538dc613
Jamey Sharp <jamey@minilop.net>
2008-11-06 09:41:23 -0800

    Minor control flow simplifications in the pressure-altitude model.

--- pressure_sensor.c
+++ pressure_sensor.c
@@ -40,55 +40,45 @@ double altitude_to_pressure(double altitude) {
    double exponent; /* exponent for function to determine pressure */
    int layer_number; /* identifies layer in the atmosphere */
    int delta_z; /* difference between two altitudes */
-   int done;
-   int i;
    
    if (altitude < 0) /* illegal altitude */
       return -1;
-   else if (altitude > MAXIMUM_ALTITUDE) /* FIX ME: use sensor data to improve model */
+   if (altitude > MAXIMUM_ALTITUDE) /* FIX ME: use sensor data to improve model */
       return 0;
-   else {
-      /* calculate the base temperature and pressure for the atmospheric layer
-         associated with the inputted altitude */
-      layer_number = 0;
-      done = 0;
-      for(i = 0; (!done && i < NUMBER_OF_LAYERS - 1); i++) {
-         if (altitude > base_altitude[layer_number + 1]) {
-            delta_z = base_altitude[layer_number + 1] - base_altitude[layer_number];
-            if (lapse_rate[layer_number] == 0.0) {
-               exponent = GRAVITATIONAL_ACCELERATION * delta_z 
-                    / AIR_GAS_CONSTANT / base_temperature;
-               base_pressure *= exp(exponent);
-            }
-            else {
-               base = (lapse_rate[layer_number] * delta_z / base_temperature) + 1.0;
-               exponent = GRAVITATIONAL_ACCELERATION / 
-                    (AIR_GAS_CONSTANT * lapse_rate[layer_number]);
-               base_pressure *= pow(base, exponent);
-            }
-            base_temperature += delta_z * lapse_rate[layer_number];
-            layer_number++;
-         }
-         else
-            done = 1;
-      }
 
-      /* calculate the pressure at the inputted altitude */
-      delta_z = altitude - base_altitude[layer_number];
+   /* calculate the base temperature and pressure for the atmospheric layer
+      associated with the inputted altitude */
+   for(layer_number = 0; layer_number < NUMBER_OF_LAYERS - 1 && altitude > base_altitude[layer_number + 1]; layer_number++) {
+      delta_z = base_altitude[layer_number + 1] - base_altitude[layer_number];
       if (lapse_rate[layer_number] == 0.0) {
          exponent = GRAVITATIONAL_ACCELERATION * delta_z 
               / AIR_GAS_CONSTANT / base_temperature;
-         pressure = base_pressure * exp(exponent);
+         base_pressure *= exp(exponent);
       }
       else {
          base = (lapse_rate[layer_number] * delta_z / base_temperature) + 1.0;
-         exponent = GRAVITATIONAL_ACCELERATION /
+         exponent = GRAVITATIONAL_ACCELERATION / 
               (AIR_GAS_CONSTANT * lapse_rate[layer_number]);
-         pressure = base_pressure * pow(base, exponent);
-      } 
-
-      return pressure;
+         base_pressure *= pow(base, exponent);
+      }
+      base_temperature += delta_z * lapse_rate[layer_number];
    }
+
+   /* calculate the pressure at the inputted altitude */
+   delta_z = altitude - base_altitude[layer_number];
+   if (lapse_rate[layer_number] == 0.0) {
+      exponent = GRAVITATIONAL_ACCELERATION * delta_z 
+           / AIR_GAS_CONSTANT / base_temperature;
+      pressure = base_pressure * exp(exponent);
+   }
+   else {
+      base = (lapse_rate[layer_number] * delta_z / base_temperature) + 1.0;
+      exponent = GRAVITATIONAL_ACCELERATION /
+           (AIR_GAS_CONSTANT * lapse_rate[layer_number]);
+      pressure = base_pressure * pow(base, exponent);
+   } 
+
+   return pressure;
 }
 
 
@@ -111,48 +101,47 @@ double pressure_to_altitude(double pressure) {
 
    if (pressure < 0 || pressure > LAYER0_BASE_PRESSURE)  /* illegal pressure */
       return -1;
-   else if (pressure < MINIMUM_PRESSURE) /* FIX ME: use sensor data to improve model */
+   if (pressure < MINIMUM_PRESSURE) /* FIX ME: use sensor data to improve model */
       return MAXIMUM_ALTITUDE;
-   else {
-      /* calculate the base temperature and pressure for the atmospheric layer
-         associated with the inputted pressure. */
-      layer_number = -1;
-      do {
-         layer_number++;
-         base_pressure = next_base_pressure;
-         base_temperature = next_base_temperature;
-         delta_z = base_altitude[layer_number + 1] - base_altitude[layer_number];
-         if (lapse_rate[layer_number] == 0.0) {
-            exponent = GRAVITATIONAL_ACCELERATION * delta_z 
-                 / AIR_GAS_CONSTANT / base_temperature;
-            next_base_pressure *= exp(exponent);
-         }
-         else {
-            base = (lapse_rate[layer_number] * delta_z / base_temperature) + 1.0;
-            exponent = GRAVITATIONAL_ACCELERATION / 
-                 (AIR_GAS_CONSTANT * lapse_rate[layer_number]);
-            next_base_pressure *= pow(base, exponent);
-         }
-         next_base_temperature += delta_z * lapse_rate[layer_number];
-      }
-      while(layer_number < NUMBER_OF_LAYERS - 1 && pressure < next_base_pressure);
 
-      /* calculate the altitude associated with the inputted pressure */
+   /* calculate the base temperature and pressure for the atmospheric layer
+      associated with the inputted pressure. */
+   layer_number = -1;
+   do {
+      layer_number++;
+      base_pressure = next_base_pressure;
+      base_temperature = next_base_temperature;
+      delta_z = base_altitude[layer_number + 1] - base_altitude[layer_number];
       if (lapse_rate[layer_number] == 0.0) {
-         coefficient = (AIR_GAS_CONSTANT / GRAVITATIONAL_ACCELERATION) 
-                                                       * base_temperature;
-         altitude = base_altitude[layer_number]
-                       + coefficient * log(pressure / base_pressure);
+         exponent = GRAVITATIONAL_ACCELERATION * delta_z 
+              / AIR_GAS_CONSTANT / base_temperature;
+         next_base_pressure *= exp(exponent);
       }
       else {
-         base = pressure / base_pressure;
-         exponent = AIR_GAS_CONSTANT * lapse_rate[layer_number] 
-                                          / GRAVITATIONAL_ACCELERATION;
-         coefficient = base_temperature / lapse_rate[layer_number];
-         altitude = base_altitude[layer_number]
-                         + coefficient * (pow(base, exponent) - 1);
+         base = (lapse_rate[layer_number] * delta_z / base_temperature) + 1.0;
+         exponent = GRAVITATIONAL_ACCELERATION / 
+              (AIR_GAS_CONSTANT * lapse_rate[layer_number]);
+         next_base_pressure *= pow(base, exponent);
       }
-
-      return altitude;
+      next_base_temperature += delta_z * lapse_rate[layer_number];
    }
+   while(layer_number < NUMBER_OF_LAYERS - 1 && pressure < next_base_pressure);
+
+   /* calculate the altitude associated with the inputted pressure */
+   if (lapse_rate[layer_number] == 0.0) {
+      coefficient = (AIR_GAS_CONSTANT / GRAVITATIONAL_ACCELERATION) 
+                                                    * base_temperature;
+      altitude = base_altitude[layer_number]
+                    + coefficient * log(pressure / base_pressure);
+   }
+   else {
+      base = pressure / base_pressure;
+      exponent = AIR_GAS_CONSTANT * lapse_rate[layer_number] 
+                                       / GRAVITATIONAL_ACCELERATION;
+      coefficient = base_temperature / lapse_rate[layer_number];
+      altitude = base_altitude[layer_number]
+                      + coefficient * (pow(base, exponent) - 1);
+   }
+
+   return altitude;
 }
