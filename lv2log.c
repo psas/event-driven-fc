bdc54b7c400e0b9c9472abf67b08212d4e55d2e6
Josh Triplett <josh@joshtriplett.org>
2010-03-30 21:58:44 -0700

    Parse GPS messages from Jupiter board and pass ECEF to the flight computer
    Commit by Josh Triplett and Jamey Sharp.

--- lv2log.c
+++ lv2log.c
@@ -1,10 +1,13 @@
+#include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 #include <arpa/inet.h>
 
 #include "interface.h"
 #include "sim-common.h"
 
+static bool processed_message = false;
 static uint32_t last_timestamp;
 
 static double to_seconds(uint32_t timestamp)
@@ -46,6 +49,55 @@ static uint16_t read16(const unsigned char *buf)
 	return (uint16_t) buf[0] << 8 | buf[1];
 }
 
+static uint16_t read16le(const uint8_t *buf)
+{
+	return (uint16_t) buf[1] << 8 | buf[0];
+}
+
+static uint32_t read32le(const uint8_t *buf)
+{
+	return (uint32_t) buf[3] << 24 | buf[2] << 16 | buf[1] << 8 | buf[0];
+}
+
+static size_t consume_gps(uint8_t gps_buffer[], size_t gps_length)
+{
+	if (gps_length < 10)
+		return 0;
+	if (gps_buffer[0] != 0xFF || gps_buffer[1] != 0x81)
+		return 1;
+	uint16_t sum = 0;
+	size_t sum_word;
+	for (sum_word = 0; sum_word < 5; ++sum_word)
+		sum += read16le(gps_buffer + sum_word * 2);
+	if (sum != 0)
+		return 2;
+	uint16_t word_count = read16le(gps_buffer + 4) + 1;
+	if (gps_length < 10U + word_count * 2U)
+		return 0;
+	for (; sum_word < 5U + word_count; ++sum_word)
+		sum += read16le(gps_buffer + sum_word * 2);
+	if (sum != 0)
+		return 2;
+	switch (read16le(gps_buffer + 2))
+	{
+	case 1009: ;
+		vec3 pos = {{
+			.x = (int32_t) read32le(gps_buffer + 18) / 100.0,
+			.y = (int32_t) read32le(gps_buffer + 22) / 100.0,
+			.z = (int32_t) read32le(gps_buffer + 26) / 100.0,
+		}};
+		vec3 vel = {{
+			.x = (int32_t) read32le(gps_buffer + 30) / 100.0,
+			.y = (int32_t) read32le(gps_buffer + 34) / 100.0,
+			.z = (int32_t) read32le(gps_buffer + 38) / 100.0,
+		}};
+		gps_sensor(pos, vel);
+		processed_message = true;
+		break;
+	}
+	return 10 + 2 * word_count;
+}
+
 struct canmsg_t {
 	uint32_t        id;             /* id<<5 + rtr<<4 + len */
 	uint32_t        timestamp;
@@ -64,7 +116,8 @@ int main(int argc, const char *const argv[])
 	init(initial_geodetic);
 
 	struct canmsg_t msg;
-	bool processed_message = false;
+	uint8_t gps_buffer[4096];
+	size_t gps_length = 0;
 	while(fread(&msg, sizeof(msg), 1, stdin) == 1)
 	{
 		msg.id = ntohl(msg.id);
@@ -76,6 +129,7 @@ int main(int argc, const char *const argv[])
 			last_timestamp = msg.timestamp;
 			processed_message = false;
 		}
+		size_t length = msg.id & 0xF;
 		switch(msg.id)
 		{
 		case /* FC_REQUEST_STATE */ 0x0021:
@@ -106,6 +160,16 @@ int main(int argc, const char *const argv[])
 				trace_printf("LV2 fired drogue chute pyro\n");
 			if(msg.data[0] == 3)
 				trace_printf("LV2 fired main chute pyro\n");
+			break;
+		case /* GPS_UART_TRANSMIT */ 0x5301 ... 0x5308:
+			assert (gps_length + length < sizeof(gps_buffer));
+			memcpy(gps_buffer + gps_length, msg.data, length);
+			gps_length += length;
+			while((length = consume_gps(gps_buffer, gps_length)))
+			{
+				gps_length -= length;
+				memmove(gps_buffer, gps_buffer + length, gps_length);
+			}
 			break;
 		}
 	}
